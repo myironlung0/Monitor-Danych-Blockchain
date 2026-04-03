54c7fc8a37a9e84964e0f73247f27f07d7ab0fe0
Kowalczuk <wkowalczuk03@gmail.com>
2026-04-03 20:28:58 +0200

    Add node URL loading from config.properties

--- src/main/java/pl/blockcraft/App.java
+++ src/main/java/pl/blockcraft/App.java
@@ -4,16 +4,19 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import pl.blockcraft.access.Web3jProvider;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
  * MAIN CLASS
  *
  */
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
+    // TODO: obsłuyć błędy
+    public static void main( String[] args ) throws IOException {
         Web3j web3j = Web3jProvider.connect();
         try {
             Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
--- src/main/java/pl/blockcraft/access/Web3jProvider.java
+++ src/main/java/pl/blockcraft/access/Web3jProvider.java
@@ -9,12 +9,39 @@ Web3j connection configuration
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Web3jProvider {
 
-    // TO DO: przerzucic url do config file
-    private static final String NODE_URL = "https://eth-sepolia.g.alchemy.com/v2/JkVjv2trp9Nb1-2PGqps5";
+    private static final String CONFIG_FILE = "config.properties";
 
-    public static Web3j connect() {
-        return Web3j.build(new HttpService(NODE_URL));
+    // TODO: obsłuyć błędy
+
+    public static Web3j connect() throws IOException {
+        return Web3j.build(new HttpService(getNodeUrl()));
+    }
+
+    // TODO: obsłuyć błędy
+    public static String getNodeUrl() throws IOException {
+        Properties props = new Properties();
+
+
+        try(InputStream input = Web3jProvider.class.getClassLoader().getResourceAsStream(CONFIG_FILE)) {
+            if (input == null) {
+                throw new IllegalStateException("FILE NOT FOUND " + CONFIG_FILE);
+            }
+            props.load(input);
+            input.close();
+
+            String url = props.getProperty("ethereum.node.url");
+            if (url == null) {
+                throw new IllegalStateException("NO KEY 'ethereum.node.url' IN " + CONFIG_FILE);
+            }
+            return url;
+        }
     }
 }
