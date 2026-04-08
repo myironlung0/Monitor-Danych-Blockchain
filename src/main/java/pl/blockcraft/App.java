cc99c3836f0c242e00610642217c8c6a2518a033
Kowalczuk <wkowalczuk03@gmail.com>
2026-04-08 18:03:38 +0200

    add missing app

--- src/main/java/pl/blockcraft/App.java
+++ src/main/java/pl/blockcraft/App.java
@@ -1,10 +1,12 @@
 package pl.blockcraft;
 
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.EthBlock;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import pl.blockcraft.access.BlockFetcher;
 import pl.blockcraft.access.Web3jProvider;
 
-import java.io.FileNotFoundException;
+import java.util.List;
 import java.io.IOException;
 
 /**
@@ -12,17 +14,22 @@ import java.io.IOException;
  *
  */
 
-
 public class App 
 {
     // TODO: obsłuyć błędy
     public static void main( String[] args ) throws IOException {
         Web3j web3j = Web3jProvider.connect();
+        BlockFetcher fetcher = new BlockFetcher(web3j);
+
         try {
             Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
             String clientVersion = web3ClientVersion.getWeb3ClientVersion();
 
             System.out.println("Connection successful, client version: " + clientVersion);
+
+            EthBlock.Block block = fetcher.getLatestBlock();
+
+            List<EthBlock.Block> blockList = fetcher.getLatestBlocks(100);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
