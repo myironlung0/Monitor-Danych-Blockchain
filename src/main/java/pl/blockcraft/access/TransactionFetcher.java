86c06ccc778c7ca1d4328e17f245f833e9adabb3
Kowalczuk <wkowalczuk03@gmail.com>
2026-05-07 16:00:15 +0200

    Add simple rate limiting

--- src/main/java/pl/blockcraft/access/TransactionFetcher.java
+++ src/main/java/pl/blockcraft/access/TransactionFetcher.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 public class TransactionFetcher implements TransactionFetcherInterface{
     private final Web3j web3j;
+    private static final int MAX_RETRIES = 3;
 
     public TransactionFetcher(Web3j web3j) {
         this.web3j = web3j;
@@ -19,17 +20,32 @@ public class TransactionFetcher implements TransactionFetcherInterface{
 
     public List<EthBlock.TransactionObject> getTransactionList(BigInteger blockNum) throws BlockchainDataException{
         List<EthBlock.TransactionObject> transactionList = new ArrayList<>();
-        try {
-            List<EthBlock.TransactionResult> txs = web3j.ethGetBlockByNumber(
-                    new DefaultBlockParameterNumber(blockNum), true).send().getBlock().getTransactions();
+        for(int i = 1; i <= MAX_RETRIES; i++) {
+            try {
+                List<EthBlock.TransactionResult> txs = web3j.ethGetBlockByNumber(
+                        new DefaultBlockParameterNumber(blockNum), true).send().getBlock().getTransactions();
 
-            for(EthBlock.TransactionResult result: txs) {
-                transactionList.add((EthBlock.TransactionObject)result.get());
+                for(EthBlock.TransactionResult result: txs) {
+                    transactionList.add((EthBlock.TransactionObject)result.get());
+                }
+
+                return transactionList;
+            }catch(IOException e) {
+                if (i == MAX_RETRIES) {
+                    throw new BlockchainDataException("Failed to fetch transactions for block " + blockNum, e);
+                }
+
+                try{
+                    Thread.sleep((long) Math.pow(2, i) * 1000L);
+                }catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt(); // restore the flag
+                    throw new BlockchainDataException("Interrupted while retrying transactions fetch", ie);
+                }
+
+                System.err.println("Attempt " + i + " failed for block " + blockNum + ", retrying...");
             }
-        }catch(IOException e) {
-            throw new BlockchainDataException("Failed to fetch transactions for block" + blockNum, e);
         }
-        return transactionList;
+        throw new BlockchainDataException("Failed to fetch transactions for block" + blockNum + " after " + MAX_RETRIES + " attempts", null);
     }
 
     // returns transactions from n most recent blocks
@@ -39,12 +55,14 @@ public class TransactionFetcher implements TransactionFetcherInterface{
             BigInteger latestBlockNum = blockFetcher.getLatestBlock().getNumber();
 
             for (int i = 0; i < n; i++) {
+                Thread.sleep(100);
                 BigInteger blockNum = latestBlockNum.subtract(BigInteger.valueOf(i));
                 List<EthBlock.TransactionObject> txs = getTransactionList(blockNum);
                 allTransactions.addAll(txs);
-                //System.out.println("Pobrano " + txs.size() + " transakcji z bloku #" + blockNum);
             }
-        } catch (BlockchainDataException e) {
+        }catch(InterruptedException ie){
+            throw new BlockchainDataException("Failed to fetch transactions from latest blocks. Interrpupted while retrying", ie);
+        }catch (BlockchainDataException e) {
             throw new BlockchainDataException("Failed to fetch transactions from latest blocks", e);
         }
 
