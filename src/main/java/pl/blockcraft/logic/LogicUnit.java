9b9df1e2b92841290243046007ed7a6b34eeb467
bspat <154008056+bspat@users.noreply.github.com>
2026-05-14 20:55:08 +0200

    Implement LogicUnit

---
+++ src/main/java/pl/blockcraft/logic/LogicUnit.java
@@ -0,0 +1,140 @@
+package pl.blockcraft.logic;
+
+import org.web3j.protocol.core.methods.response.EthBlock;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LogicUnit implements LogicInterface
+{
+
+    @Override
+    public BigInteger getNumberOfBlocks(List<EthBlock.Block> blocks)
+    {
+        return BigInteger.valueOf(blocks.size());
+    }
+
+    @Override
+    public BigInteger getNumberOfTransactions(EthBlock.Block block)
+    {
+        return BigInteger.valueOf(block.getTransactions().size());
+    }
+
+    @Override
+    public List<BigInteger> getNumberOfTransactions(List<EthBlock.Block> blocks)
+    {
+        List<BigInteger> result = new ArrayList<>();
+        for(EthBlock.Block block : blocks){
+            result.add(getNumberOfTransactions(block));
+        }
+        return result;
+    }
+
+    @Override
+    public BigInteger getTotalNumberOfTransactions(List<EthBlock.Block> blocks)
+    {
+        BigInteger result = BigInteger.ZERO;
+        for(EthBlock.Block block : blocks){
+            result = result.add(getNumberOfTransactions(block));
+        }
+        return result;
+    }
+
+    @Override
+    public BigInteger getGas(EthBlock.Block block)
+    {
+        return block.getGasUsed();
+    }
+
+    @Override
+    public List<BigInteger> getGas(List<EthBlock.Block> blocks)
+    {
+        List<BigInteger> result = new ArrayList<>();
+        for(EthBlock.Block block : blocks){
+            result.add(getGas(block));
+        }
+        return result;
+    }
+
+    @Override
+    public BigInteger getTotalGas(List<EthBlock.Block> blocks)
+    {
+        BigInteger result = BigInteger.ZERO;
+        for(EthBlock.Block block : blocks){
+            result = result.add(getGas(block));
+        }
+        return result;
+    }
+
+    @Override
+    public BigInteger getAverageGas(List<EthBlock.Block> blocks)
+    {
+        int number = 0;
+        BigInteger result = BigInteger.ZERO;
+        for(EthBlock.Block block : blocks){
+            result = result.add(getGas(block));
+        }
+        return result.divide(BigInteger.valueOf(number));
+    }
+
+    @Override
+    public BigInteger getValueOfTransaction(EthBlock.TransactionObject transaction)
+    {
+        return transaction.getValue();
+    }
+
+    @Override
+    public List<BigInteger> getValueOfTransactions(List<EthBlock.TransactionObject> transactions)
+    {
+        List<BigInteger> result = new ArrayList<>();
+        for(EthBlock.TransactionObject transaction : transactions){
+            result.add(getValueOfTransaction(transaction));
+        }
+        return result;
+    }
+
+    @Override
+    public BigInteger getTotalValueOfTransactions(List<EthBlock.TransactionObject> transactions)
+    {
+        BigInteger result = BigInteger.ZERO;
+        for(EthBlock.TransactionObject transaction : transactions){
+            result = result.add(getValueOfTransaction(transaction));
+        }
+        return result;
+    }
+
+    @Override
+    public BigInteger getTotalValueOfTransactions(EthBlock.Block block)
+    {
+        BigInteger result = BigInteger.ZERO;
+        List<EthBlock.TransactionResult> lista = block.getTransactions();
+        for(EthBlock.TransactionResult transaction : lista){
+            result = result.add(getValueOfTransaction((EthBlock.TransactionObject)transaction));
+        }
+        return result;
+    }
+
+    @Override
+    public BigInteger getTotalValueOfTransactionsInBlocks(List<EthBlock.Block> blocks)
+    {
+        BigInteger result = BigInteger.ZERO;
+        for(EthBlock.Block block : blocks){
+            result = result.add(getTotalValueOfTransactions(block));
+        }
+        return result;
+    }
+
+    @Override
+    public String getSender(EthBlock.TransactionObject transaction)
+    {
+        return transaction.getFrom();
+    }
+
+    @Override
+    public String getReceiver(EthBlock.TransactionObject transaction)
+    {
+        return transaction.getTo();
+    }
+}
